aaa61cac51b932dcdb4af40b7801766f4483ff59
Adam Bowen <adambowen@group360.com>
2013-05-14 14:46:52 -0500

    Add basic capybara tests for Login form

---
+++ spec/views/devise/sessions/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "Login form" do
+    before :each do
+        visit '/login'
+    end
+    it 'should have the proper labels' do
+        expect(page).to have_content('Email')
+        expect(page).to have_content('Password')
+    end
+
+    it 'should have the "Forgot your password?" link' do
+        # have_link is stupid, and requires the link text.  However,
+        # that seems a bit overly-fragile to me, so I'm omitting it.
+        expect(page).to have_link('', href: '/users/password/new')
+    end
+
+    it 'should have the "Register" link' do
+        expect(page).to have_link('', href: '/register')
+    end
+end
