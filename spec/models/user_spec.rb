56f609f70c2b73eebabf000356e951a7746b43e4
Edu <eduerondu@gmail.com>
2013-05-15 15:27:20 -0500

    add unit test to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:dan) {{
+    email: "dan@example.com",
+    password: "secretpassword"
+    }}
+
+    before(:each) do
+      @dan = User.new dan
+    end
+    it "should be valid" do
+        @dan.should be_valid
+    end
+
+    it "should not valid without email" do
+      @dan.email = nil
+      @dan.should_not be_nil
+    end
+
+    it "should not be vaild without password" do
+        @dan.password = nil
+        @dan.should_not be_nil
+    end
 end
