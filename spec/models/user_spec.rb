d4b42ef7dedf54d6dbf4ed626ed467429dbcd036
Edu <eduerondu@gmail.com>
2013-05-15 17:21:34 -0500

    improve user_spec.rb use a subject for test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  let(:dan) {{
-    email: "dan@example.com",
-    password: "secretpassword"
-    }}
+  subject(:dan) { User.new(
+      email: "dan@example.com",
+      password: "secretpassword"
+      )}
 
-    before(:each) do
-      @dan = User.new dan
-    end
+    it { should be_valid }
     it "should be valid" do
-        @dan.should be_valid
+        subject.should be_valid
     end
 
     it "should not valid without email" do
-      @dan.email = nil
-      @dan.should_not be_nil
+      subject.email = nil
+      subject.should_not be_nil
     end
 
     it "should not be vaild without password" do
-        @dan.password = nil
-        @dan.should_not be_nil
+        subject.password = nil
+        subject.should_not be_nil
     end
 end
