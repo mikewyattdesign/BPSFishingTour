c1f36722f458cb28dc3d5c1f72d6eb20e799bbf9
Edu <eduerondu@gmail.com>
2013-05-15 13:52:31 -0500

    FISHING - 34 #RESOLVED - password reset email works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,21 @@ BpsFishingTour::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.default_options = {from: 'eduerondu@gmail.com'}
+
+  # mail server setup
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     address: "smtp.gmail.com",
+     port: 587,
+     domain: 'myschoolbeacon.com',
+     authentication: 'plain',
+     user_name: 'group360worldwide@gmail.com',
+     password: '1227WashAve!',
+     enable_starttls_auto: true
+   }
+   # puts "#{EMAIL}"
+
 end
