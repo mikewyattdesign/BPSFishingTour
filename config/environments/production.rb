1dba2c4b20eb922c6bc6c41c8e7e9ad115220170
Edu <eduerondu@gmail.com>
2013-05-16 14:36:12 -0500

    fix send grid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ BpsFishingTour::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.default_url_options = { :host => 'ishing-tour-dev.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
