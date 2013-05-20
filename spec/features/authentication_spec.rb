27952331aa202276000f3ea3bfe7509536a10506
Edu <eduerondu@gmail.com>
2013-05-20 16:02:20 -0500

    make sure test are testing the right things

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -5,20 +5,22 @@ feature "authentication" do
     email: "dan@example.com",
     password: "secretpassword"
       )}
+    # Joe is our register and confirmed guy
+    let(:joe) { User.create(email: "eduerondu@gmail.com", password: "supersecrety", confirmed_at: "2013-05-20 20:49:50")}
 
-  scenario "registered user logs-in successfully" do
+  # scenario "registered user logs-in successfully" do
 
-    sign_up_with(dan.email, dan.password)
+  #   sign_up_with(dan.email, dan.password)
 
-    pending 'Where should users go?'
+  #   pending 'Where should users go?'
+  # end
+
+  scenario "registered and confirmed user logs-in to site" do
+    sign_in_with joe.email, joe.password
+    expect(current_path).to eq '/'
   end
 
-  scenario "user registers for the site" do
-    sign_in_with dan.email, dan.password
-    expect(current_path).to eq '/confirm'
-  end
-
-  scenario "user should receive registration email" do
+  scenario "user can register and confirm account" do
     ActionMailer::Base.deliveries.clear
     sign_up_with(dan.email, dan.password)
     expect(ActionMailer::Base.deliveries.size).to eq 1
@@ -31,8 +33,8 @@ feature "authentication" do
   end
 
 
-  def sign_up_with(email, password)
-    dan.save
+  def sign_in_with(email, password)
+    sign_up_with(email, password)
     visit '/'
 
     click_link :Login
@@ -44,7 +46,7 @@ feature "authentication" do
     click_button "Login"
   end
 
-  def sign_in_with(email, password)
+  def sign_up_with(email, password)
     visit '/'
 
     click_link "Sign up"
