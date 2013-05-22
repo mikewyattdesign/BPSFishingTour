788be20f0fda7efffa2ac9bf9860521c45f5a9df
Edu Erondu <eduerondu@gmail.com>
2013-05-21 22:55:43 -0500

    change routing for successful sign in

--- spec/support/features/sessions_helpers.rb
+++ spec/support/features/sessions_helpers.rb
@@ -1,30 +1,29 @@
 module Features
-    module SessionHelpers
-      def sign_in_with(email, password)
-        # sign_up_with(email, password)
-        visit '/'
+  module SessionHelpers
+    def sign_in_with(email, password)
+      # sign_up_with(email, password)
+      visit '/'
 
-        click_link :Login
+      click_link :Login
 
-        fill_in :Email, with: email
+      fill_in :Email, with: email
 
-        fill_in :Password, with: password
+      fill_in :Password, with: password
 
-        click_button "Login"
-      end
-
-      def sign_up_with(email, password)
-        visit '/'
-
-        click_link "Sign up"
-
-        fill_in :Email, with: email
-
-        fill_in :Password, with: password
-
-        fill_in "Password confirmation", with: password
-
-        click_button :GO
-      end
+      click_button "Login"
     end
+
+    def sign_up_with(email, password)
+      visit '/'
+
+      click_link "Sign up"
+
+      fill_in :Email, with: email
+
+      fill_in :Password, with: password
+
+      fill_in "Password confirmation", with: password
+      click_button :GO
+    end
+  end
 end
