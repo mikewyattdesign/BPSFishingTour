6c6bcea375883e78212b437980c61584fdffad50
Edu <eduerondu@gmail.com>
2013-05-22 11:49:16 -0500

    create Profiles factory

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+    factory :profile, aliases: [:user_profile] do
+        first_name    "Joe"
+        last_name     "Daggrassi"
+        street_address "1234 Main Street"
+        city           "St. Louis"
+        zip            "63112"
+        phone          "5555551234"
+    end
+end
