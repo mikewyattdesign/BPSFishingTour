2629b3cbac21b673de1bf359cb3d999809866027
Edu Erondu <eduerondu@gmail.com>
2013-05-21 22:54:10 -0500

    remove mailer test from feature specs

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,41 +1,30 @@
 require 'spec_helper'
 
 feature "authentication" do
-  subject(:dan) { User.new(
-    email: "dan@example.com",
-    password: "secretpassword"
-      )}
-    # Joe is our register and confirmed guy
-    let(:joe) { User.create(email: "eduerondu@gmail.com", password: "supersecrety", confirmed_at: "2013-05-20 20:49:50")}
-
-  # scenario "registered user logs-in successfully" do
-
-  #   sign_up_with(dan.email, dan.password)
-
-  #   pending 'Where should users go?'
-  # end
+  subject = FactoryGirl.attributes_for(:user)
+  before(:each) do
+    @joe = OpenStruct.new(subject)
+  end
 
   scenario "registered and confirmed user logs-in to site" do
-    sign_in_with joe.email, joe.password
+    FactoryGirl.create(:user)
+    sign_in_with @joe.email, @joe.password
     expect(current_path).to eq '/'
-
   end
 
   scenario "user can register and confirm account" do
-    ActionMailer::Base.deliveries.clear
-    sign_up_with(dan.email, dan.password)
-    expect(ActionMailer::Base.deliveries.size).to eq 1
-    expect(ActionMailer::Base.deliveries.last).to have_content("Welcome dan@example.com!")
-    expect(ActionMailer::Base.deliveries.last).to have_link('Confirm my account')
-    token = ActionMailer::Base.deliveries.last.to_s.match(/confirmation_token=(.+)"/)[1]
-
+    sign_up_with(@joe.email, @joe.password)
+    last_email.to.should include @joe.email
+    last_email.body.should have_content("Welcome joe@example.com!")
+    last_email.body.should have_link('Confirm my account')
+    token = last_email.to_s.match(/confirmation_token=(.+)"/)[1]
     visit "/users/confirmation?confirmation_token=#{token}"
     expect(current_path).to eq('/thanks')
+    puts current_path
   end
 
   scenario "user attempts to log in with wrong password" do
-    sign_in_with joe.email, "wrongeffingpw"
-
+    sign_in_with @joe.email, "wrongeffingpw"
     expect(current_path).to  eq('/users/sign_in')
     expect(page).to have_content("Invalid email or password")
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -43,4 +44,6 @@ RSpec.configure do |config|
   # Add the Capybara DSL
   config.include Capybara::DSL
   config.include Features::SessionHelpers, type: :feature
+  config.include(MailerMacros)
+  config.before(:each) { reset_email }
 end
