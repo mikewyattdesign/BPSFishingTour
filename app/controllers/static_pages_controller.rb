09a4a6e6ff39188dcfd6a422a3d47199350363b8
Eric Suh <ericsuh@group360.com>
2013-05-23 12:14:43 -0500

    CSS For Alert

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,10 @@ class StaticPagesController < ApplicationController
   end
 
   def thanks
+    flash[:notice] = "notice message"
+    flash[:alert] = "alert message"
+    flash[:error] = "error message"
+    flash[:success] = "success message"
   end
 
   def confirm
