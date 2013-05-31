75afd3f0e50765c8d25e8a630fa1956f1f84cdc7
Edu <eduerondu@gmail.com>
2013-05-31 15:03:24 -0500

    fix precompile issue

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module BpsFishingTour
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
   end
