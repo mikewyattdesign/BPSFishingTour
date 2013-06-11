9b09cedf6990027ae64d791436d18368e45c855d
Edu <eduerondu@gmail.com>
2013-06-11 14:26:19 -0500

    get rid of static events page and route

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ BpsFishingTour::Application.routes.draw do
     get '/thanks' => 'static_pages#thanks'
     get '/confirm' => 'static_pages#confirm'
 
-    get '/events' => 'static_pages#events'
-
-
     devise_for :users, controllers: {
         registrations: 'registrations',
         confirmations: 'confirmations'
