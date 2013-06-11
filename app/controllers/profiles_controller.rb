63110aeb8f10a1977728d10926ddd93b9648fc44
Edu <eduerondu@gmail.com>
2013-06-11 16:30:41 -0500

    add ruby team events partial to Profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,6 +10,11 @@ class ProfilesController < ApplicationController
   # GET /profiles/1
   # GET /profiles/1.json
   def show
+    if(current_user.teams.size > 0)
+      @can_register = true
+    else
+      @can_register = false
+    end
     @profile = current_user.profile
     @bootstrap = true
   end
