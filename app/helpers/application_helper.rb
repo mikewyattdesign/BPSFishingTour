f7fa2cc02f423ee564dd839877ac3f0be9748a40
Edu <eduerondu@gmail.com>
2013-06-18 10:07:09 -0500

    FISHING-153 - add is_registered? helper #RESOLVED

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
     def has_team?
         current_user.teams.exists?
     end
+
+    def is_registered?
+        if current_user.profile.created_at != current_user.profile.updated_at
+            return true
+        else
+            return false
+        end
+    end
 end
