67bdd737696fa53b1d4fe606db6d980d38c75890
Adam Bowen <adambowen@group360.com>
2013-06-20 16:38:57 -0500

    Fix "Create your profile" button link on sign up
    FISHING-204 #resolve #time 15m

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,17 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+    def home
+    end
 
-  def about
-  end
+    def about
+    end
 
-  def events
-  end
+    def events
+    end
 
-  def thanks
-  end
+    def thanks
+        @user = current_user
+    end
 
-  def confirm
-  end
+    def confirm
+    end
 end
