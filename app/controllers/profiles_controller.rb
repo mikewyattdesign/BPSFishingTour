489dcc592f03d99c6c278ec8a2521004c4f043b5
Scott McRae <scottmcrae@group360.com>
2013-06-23 12:50:28 -0500

    add devise user_signed_in to if statement to correct error of clicking on your own profile while not currently logged in
    Signed-off-by: Scott McRae <scottmcrae@group360.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,7 +11,7 @@ class ProfilesController < ApplicationController
     # GET /profiles/1.json
     def show
         @profile = Profile.find(params[:id])
-        redirect_to my_profile_path if @profile.id == current_user.profile.id
+        redirect_to my_profile_path if user_signed_in? && @profile.id == current_user.profile.id
         @can_register = @profile.user.teams.size > 0
     end
 
