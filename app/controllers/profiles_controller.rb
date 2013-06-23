acc9e2d4197b522ee277fd4ef746555a49a675de
Scott McRae <scottmcrae@group360.com>
2013-06-23 13:44:51 -0500

    Add "teammate" object to profile model
    Signed-off-by: Scott McRae <scottmcrae@group360.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,6 +11,7 @@ class ProfilesController < ApplicationController
     # GET /profiles/1.json
     def show
         @profile = Profile.find(params[:id])
+        @teammate = @profile.user.teammate
         redirect_to my_profile_path if user_signed_in? && @profile.id == current_user.profile.id
         @can_register = @profile.user.teams.size > 0
     end
