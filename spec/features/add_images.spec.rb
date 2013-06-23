732c8d44c28b819dbfc0ae391d6844ec35d328eb
Edu <eduerondu@gmail.com>
2013-06-23 09:56:47 -0500

    trying to get this spec to do what it should do.
     - doesn't seem to save the picture for the controller to retrieve it!

--- spec/features/add_images.spec.rb
+++ spec/features/add_images.spec.rb
@@ -12,11 +12,11 @@ feature "Add Images" do
             # jane adds a profile picture
             bob.profile.picture = File.open(Rails.root.join('spec', 'features', 'files', 'images.jpeg'))
             bob.save(validate: false).should be_true
+            puts bob.vaild?
             # puts bob.profile.picture?
         }
 
         scenario "try to upload a team photo" do
-            puts bob.profile.as_json
             sign_in_with(bob.email, bob.password)
             expect(current_path).to eq "/myprofile"
             # puts bob.teams.first.users
@@ -25,7 +25,6 @@ feature "Add Images" do
             page.body.should_not have_link "find a teammate!"
             url = bob.profile.picture.url(:tour)
             # puts page.body
-            puts Profile.count
             page.body.should have_xpath("//img[@src=\"#{url}\"]")
 
         end
