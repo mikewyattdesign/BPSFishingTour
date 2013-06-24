21f019ee575470b5c9785eb405131a0f52f9cbec
Edu <eduerondu@gmail.com>
2013-06-24 15:22:11 -0500

    fix mailer url issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ BpsFishingTour::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'fishing-tour-dev.herokuapp.com' }
-  config.action_mailer.asset_host = 'http://fishing-tour-dev.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'fishing-tour.herokuapp.com' }
+  config.action_mailer.asset_host = 'http://fishing-tour.herokuapp.com'
 
 end
