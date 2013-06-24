56bf2fe4a733810dc0c1db610b55ff4802a9d1a1
Adam Bowen <adambowen@group360.com>
2013-06-24 15:59:32 -0500

    Display and order by date FISHING-259 #time 10m

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.order('date')
     @divisions = Division.all
   end
 
