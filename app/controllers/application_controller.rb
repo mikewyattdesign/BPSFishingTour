60dcd5798fdcf37ec4d6e11f44e50e54c5f72671
Edu <eduerondu@gmail.com>
2013-06-24 16:54:05 -0500

    place html links in the notifications FISHING-211 #RESOLVED

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     before_filter :please_register
+    before_filter :get_teammember
   # after_filter :any_invites?
 
     def after_sign_in_path_for(resource)
         return team_invitations_path(current_user) if session.delete(:respond_to_invitation)
         if !current_user.profile
-            "/thanks"
-        else
-            if current_user.teams.empty?
-                flash[:notice] = "What's next? Find a teammate so you two can join some tournaments! <a href=\"/teams/requests/new\">Grab A Teammate</a>"
-                return "/myprofile"
-            end
             "/myprofile"
         end
     end
 
     def please_register
         unless !user_signed_in? || view_context.is_registered?
-            flash[:please_register] = 'You have not completed your registration, please navigate to \'Register\' to do so!'
+            flash[:please_register] = %Q[You have not completed your registration, please navigate to  <a href=\\"/profiles/#{current_user.profile.id}/edit\\">the registration page</a> to do so!]
+        end
+    end
+
+    def get_teammember
+        unless current_user.teammate
+            flash[:find_a_teammate] = %Q[So, whats next? Find a teammate so you can register for tournament events! <a href=\\"/teams/requests/new\\">Grab A Teammate</a>]
         end
     end
 end
- ##{ActionController::Base.helpers.link_to "the registration page", edit_profile_path(current_user.profile)}
+
