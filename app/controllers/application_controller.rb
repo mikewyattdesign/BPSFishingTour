99eaaea9c5fac41abf956bbec8dc30809827b3bc
Edu <eduerondu@gmail.com>
2013-06-24 17:11:59 -0500

    sign in errror fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     before_filter :please_register
-    before_filter :get_teammember
+    # before_filter :get_teammember
   # after_filter :any_invites?
 
     def after_sign_in_path_for(resource)
         return team_invitations_path(current_user) if session.delete(:respond_to_invitation)
-        if !current_user.profile
             "/myprofile"
-        end
     end
 
     def please_register
@@ -20,10 +18,10 @@ class ApplicationController < ActionController::Base
         end
     end
 
-    def get_teammember
-        unless current_user.teammate
-            flash[:find_a_teammate] = %Q[So, whats next? Find a teammate so you can register for tournament events! <a href=\\"/teams/requests/new\\">Grab A Teammate</a>]
-        end
-    end
+    # def get_teammember
+    #     if current_user && current_user.teammate
+    #         flash[:find_a_teammate] = %Q[So, whats next? Find a teammate so you can register for tournament events! <a href=\\"/teams/requests/new\\">Grab A Teammate</a>]
+    #     end
+    # end
 end
 
