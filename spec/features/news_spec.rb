8022ec3bca9226c1de57d5469bc4d67a64c746f6
Edu <eduerondu@gmail.com>
2013-06-25 13:54:39 -0500

    fix news spec

--- spec/features/news_spec.rb
+++ spec/features/news_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 feature 'News' do
-    scenario 'I want to view all current anglers in the system' do
+    scenario 'I want to view news in the system' do
         visit '/news'
-        expect(page).to have_content 'news'
+        expect(page).to have_content 'News'
     end
 end
