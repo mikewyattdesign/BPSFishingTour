3c8aa0f622098539e2d5a56df81fa9e4928eeeae
Edu <eduerondu@gmail.com>
2013-06-25 14:04:06 -0500

    and unregister for event example to specs

--- spec/features/add_events_spec.rb
+++ spec/features/add_events_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 feature "Manage events" do
     subject {FactoryGirl.create(:user)}
     let(:team) {FactoryGirl.create(:team)}
-    before(:each) do
-      FactoryGirl.create(:event)
-    end
+    let!(:event) {FactoryGirl.create(:event)}
+
 
     scenario "User can register for a new event" do
         subject.teams << team
@@ -20,6 +19,18 @@ feature "Manage events" do
             }.to change{subject.teams.first.events.count}.from(0).to(1)
     end
 
+    scenario "User should be able to unregister an event" do
+        subject.teams << team
+        subject.my_team.events << event
+        subject.my_team.events.count.should eq 1
+        sign_in_with subject.email, subject.password
+        click_link("Tour Events")
+        expect{
+            click_button("No")
+        }.to change{subject.my_team.events.count}.from(1).to(0)
+
+    end
+
     scenario "User should not be able to register for an event with out a team" do
         subject.teams.count.should eq 0
         sign_in_with(subject.email, subject.password)
@@ -28,6 +39,7 @@ feature "Manage events" do
         expect(page).to have_content("Chippawa, Upper Niagara")
         expect(page).to_not have_content("Is your team fishing in this event?")
     end
+
 
     scenario "User should not be allowed to register an event they are already registered for" do
         subject.teams << team
