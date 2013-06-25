7ff4d075d229b32e784634435728acdcca3d1a4a
Edu <eduerondu@gmail.com>
2013-06-25 13:20:50 -0500

    created thanks page spec

--- spec/features/thanks_page_spec.rb
+++ spec/features/thanks_page_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
-describe 'Thanks page' do
-    before :each do
+feature 'Thanks page' do
+    background :each do
+        user = FactoryGirl.create(:user)
+        sign_in_with(user.email, user.password)
         visit '/thanks'
     end
 
-    it 'should be thankful' do
+    scenario 'should be thankful' do
         expect(page).to have_content('Thanks')
     end
 
-    it 'should have a team create profile link' do
+    scenario 'should have a team create profile link' do
         expect(page).to have_link('Create Your Profile')
         # pending 'test for the href on the "register team" link'
     end
 
-    it 'should have a skip team registration link' do
+    scenario 'should have a skip team registration link' do
         expect(page).to have_link('Skip')
         pending 'test for the href on the "skip team registration" link'
     end
