fba2ebeae878399b19c40ad20b97cd36a1078b98
Adam Bowen <adambowen@group360.com>
2013-06-27 17:47:14 -0500

    Go to /teams/requests/new after adding profile info

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,12 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: [:update, :destroy]
-  # GET /profiles
-  # GET /profiles.json
-  def index
-    @profiles = Profile.all
-  end
+    before_action :set_profile, only: [:update, :destroy]
+
+    # GET /profiles
+    def index
+        @profiles = Profile.all
+    end
 
     # GET /profiles/1
-    # GET /profiles/1.json
     def show
         @profile = Profile.find(params[:id])
         @user = @profile.user
@@ -28,96 +27,86 @@ class ProfilesController < ApplicationController
         @is_current_user = true
     end
 
-  # GET /profiles/new
-  def new
-    @profile = Profile.new
-  end
-
-  # GET /profiles/1/edit
-  def edit
-    @profile = current_user.profile
-  end
-
-  def select_profile_pic
-    @profile = current_user.profile
-  end
-
-  def upload_profile_pic
-    @profile = current_user.profile
-    @profile.attributes = profile_params
-    if @profile.save(validate: false)
-      redirect_to my_profile_path, notice: "Your Profile picture has been uploaded!"
-    else
-      flash.clear
-      flash.now[:notice] = "Uh oh! There seems to be an issue with uploading your image!"
-      render :select_profile_pic
+    # GET /profiles/new
+    def new
+        @profile = Profile.new
     end
-  end
 
-  # POST /profiles
-  # POST /profiles.json
-  def create
-    @profile = Profile.new(profile_params)
-    @profile.user_id = current_user.id
-
-    respond_to do |format|
-      if @profile.save
-        format.html { redirect_to requests_new_path, notice: 'Thanks for creating your profile, go ahead and set up a team!' }
-        format.json { render action: 'show', status: :created, location: @profile }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
+    # GET /profiles/1/edit
+    def edit
+        @profile = current_user.profile
     end
-  end
 
-  # PATCH/PUT /profiles/1
-  # PATCH/PUT /profiles/1.json
-  def update
-    @team = current_user.teams.first
-    respond_to do |format|
-      if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
-        format.json { render json: @profile }
-      else
-        if(current_user.teams.size > 0)
-          @can_register = true
+    def select_profile_pic
+        @profile = current_user.profile
+    end
+
+    def upload_profile_pic
+        @profile = current_user.profile
+        @profile.attributes = profile_params
+        if @profile.save(validate: false)
+            redirect_to my_profile_path, notice: "Your Profile picture has been uploaded!"
         else
-          @can_register = false
+            flash.clear
+            flash.now[:notice] = "Uh oh! There seems to be an issue with uploading your image!"
+            render :select_profile_pic
         end
-        format.html {
-          path = URI::parse(request.referrer).path
-          @showTab = true
-          flash.now[:error] = "We could not update your profile!"
-          if path == "/myprofile"
-            render action: 'show_current'
-          else
-            render action: 'edit'
-          end
-        }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /profiles/1
-  # DELETE /profiles/1.json
-  def destroy
-    @profile.destroy
-    respond_to do |format|
-      format.html { redirect_to profiles_url }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_profile
-      @profile = Profile.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def profile_params
-      params.require(:profile).permit(:user_id, :first_name, :last_name, :street_address, :suite, :city, :state, :zip, :phone, :ext, :shirt, :picture, :outdoor_rewards_number, :status)
+    # POST /profiles
+    def create
+        @profile = Profile.new(profile_params)
+        @profile.user_id = current_user.id
+
+        if @profile.save
+            redirect_to requests_new_path, notice: 'Thanks for creating your profile, go ahead and set up a team!'
+        else
+            render action: 'new'
+        end
     end
+
+    # PATCH/PUT /profiles/1
+    def update
+        @team = current_user.teams.first
+        if @profile.update(profile_params)
+            if @team.nil?
+                redirect_to requests_new_path
+            else
+                redirect_to @profile, notice: 'Profile was successfully updated.'
+            end
+        else
+            if(current_user.teams.size > 0)
+                @can_register = true
+            else
+                @can_register = false
+            end
+
+            path = URI::parse(request.referrer).path
+            @showTab = true
+            flash.now[:error] = "We could not update your profile!"
+
+            if path == "/myprofile"
+                render action: 'show_current'
+            else
+                render action: 'edit'
+            end
+        end
+    end
+
+    # DELETE /profiles/1
+    def destroy
+        @profile.destroy
+        redirect_to profiles_url
+    end
+
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_profile
+            @profile = Profile.find(params[:id])
+        end
+
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def profile_params
+            params.require(:profile).permit(:user_id, :first_name, :last_name, :street_address, :suite, :city, :state, :zip, :phone, :ext, :shirt, :picture, :outdoor_rewards_number, :status)
+        end
 end
