eab27b1f7f2ac1981706f06bc36a6f81805ef954
Adam Bowen <adambowen@group360.com>
2013-07-25 17:24:08 -0500

    Remove .json views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,14 +2,12 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
-  # GET /events.json
   def index
     @events = Event.order('date')
     @divisions = Division.all
   end
 
   # GET /events/1
-  # GET /events/1.json
   def show
     if(current_user.teams.size > 0)
       @can_register = true
@@ -28,17 +26,14 @@ class EventsController < ApplicationController
   end
 
   # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
 
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @event }
       else
         format.html { render action: 'new' }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -62,26 +57,21 @@ class EventsController < ApplicationController
   end
 
   # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
       format.html { redirect_to events_url }
-      format.json { head :no_content }
     end
   end
 
--- app/views/events/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @event, :name, :address, :address2, :city, :province, :state, :country, :start_date, :end_date, :created_at, :updated_at
