fa4ebdd4ebe54d148396745cecf21f0139b80539
Mike Wyatt II <mike@mikewyattdesign.com>
2013-07-30 11:09:33 -0500

    FISHING-367 #resolve #time 9m #comment changed styling of disabled button to have faded out green. also adjusted JS to account for swapping back to no file chosen

--- app/assets/javascripts/photoButtonDisable.js
+++ app/assets/javascripts/photoButtonDisable.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $("input:file").change(function() {
+        if ($("input:file").val() != "")
+        {
             $("input[type='submit']").attr('disabled',false).removeClass('disabled');
+        }
+        else
+        {
+            $("input[type='submit']").attr('disabled',true).addClass('disabled');
+        }
 });
 });
