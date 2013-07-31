4ad03b6323aae28a7e9015db36ff5de2f0402d3c
Mike Wyatt II <mike@mikewyattdesign.com>
2013-07-31 15:48:39 -0500

    FISHING-372 #resolve #time 15m #comment Added association to Team for having scores through events. Debating whether to use polymorphic association for scoring. Scores need to belong to teams and events.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ActiveRecord::Base
     has_and_belongs_to_many :users
     has_and_belongs_to_many :events
+    has_many :scores, through: :events
 
     picture_options = {
         styles: {
