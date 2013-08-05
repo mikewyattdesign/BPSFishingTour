5b47f54a3b09fa057e8f1a2c4ab98d65f8cabafe
Edu <eduerondu@gmail.com>
2013-08-05 15:25:07 -0500

    FISHING-391 #RESOLVED

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+$(document).on('ready page:load',function(){
     $('.close').on('click', function() { $(this).parent().hide(); });
+
+    $('.blink').css('color', '#64983e');
+    $.fn.blink = function(time) {
+        var time = typeof time == 'undefined' ? 200 : time;
+        this.hide(0).delay(time).show(0);
+    }
+    setInterval(function  () {
+        $('.blink').blink();
+    }, 800)
+
 });
