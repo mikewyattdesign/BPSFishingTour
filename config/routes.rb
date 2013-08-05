3de7b23f5dd5dccc41afcb6f0c10ad693664907d
Mike Wyatt II <mike@mikewyattdesign.com>
2013-08-05 09:23:32 -0500

    Add events_path named route

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ BpsFishingTour::Application.routes.draw do
     resources :events
     match '/events/:id/attend' => 'events#register_event', as: 'register_event', via: :post
     match '/events/:id/unattend' => 'events#unregister_event', as: 'unregister_event', via: :post
+    get '/events' => 'events#index', as: 'events_path'
 
     # Authentication Routing
     devise_for :users, controllers: {
