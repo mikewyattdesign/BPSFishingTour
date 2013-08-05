a9bde030e4de693780bdb41365deefef3d089dd2
Mike Wyatt II <mike@mikewyattdesign.com>
2013-08-05 15:07:33 -0500

    add route to direct '/profiles' to 'anglers#index'

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ BpsFishingTour::Application.routes.draw do
     post '/upload_team_pic' => 'teams#upload_team_pic'
     # Profile Routing
     get '/myprofile' => 'profiles#show_current', as: 'my_profile'
-
+    get '/profiles' => 'anglers#index'
     get '/profiles/completion-reminder' => 'profiles#completion_reminder'
     resources :profiles
 
