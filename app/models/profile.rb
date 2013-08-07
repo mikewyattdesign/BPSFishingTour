2841508b304b51bf38fe0cf906c6afa7987c1bd2
Mike Wyatt II <mike@mikewyattdesign.com>
2013-08-07 12:05:19 -0500

    FISHING-401 #resolve

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,14 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
     validates :first_name, :last_name, :city, :street_address, :state, :presence => true, :on => :update
+    validates_format_of :first_name,
+        without: /\A[ \t]+|[ \t]+\z/,
+        on: :update,
+        message: "Please remove trailing and/or leading whitespace"
+    validates_format_of :last_name,
+        without: /\A[ \t]+|[ \t]+\z/,
+        on: :update,
+        message: "Please remove trailing and/or leading whitespace"
     picture_options = {
         styles: {
             medium: '300x300>}',
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,64 @@
 require 'spec_helper'
 
 describe Profile do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should be invalid without a first name' do
+    new_profile = Profile.create
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:first_name].any?).to be_true
+  end
+
+  it 'should be invalid without a last name' do
+    new_profile = Profile.create
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:last_name].any?).to be_true
+  end
+
+  it 'should have no leading spaces in the first name' do
+    new_profile = Profile.create(first_name: " Mike")
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:first_name].any?).to be_true
+  end
+
+  it 'should have no leading spaces in the last name' do
+    new_profile = Profile.create(last_name: " Skinny")
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:last_name].any?).to be_true
+  end
+
+  it 'should have no trailing spaces in the first name' do
+    new_profile = Profile.create(first_name: "Mike ")
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:first_name].any?).to be_true
+  end
+
+  it 'should have no trailing spaces in the last name' do
+    new_profile = Profile.create(last_name: "Skinny ")
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:last_name].any?).to be_true
+  end
+
+  it 'should be invalid without a street address' do
+    new_profile = Profile.create
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:street_address].any?).to be_true
+  end
+
+  it 'should be invalid without a city' do
+    new_profile = Profile.create
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:city].any?).to be_true
+  end
+
+  it 'should be invalid without a state' do
+    new_profile = Profile.create
+    expect(new_profile.invalid?).to be_true
+    expect(new_profile.errors[:state].any?).to be_true
+  end
+
+  it 'should respond to :user' do
+    new_profile = FactoryGirl.create(:profile)
+    expect(new_profile).to respond_to(:user)
+  end
+
 end
