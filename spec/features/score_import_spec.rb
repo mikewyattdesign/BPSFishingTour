cb74184f5647488118f54a92eb2920a47227d4c5
Adam Bowen <adambowen@group360.com>
2013-08-08 18:09:12 -0500

    Flesh out score import tests a bit more

--- spec/features/score_import_spec.rb
+++ spec/features/score_import_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 feature 'Score Import' do
-    given! (:event) { FactoryGirl.create(:event) }
-    given (:scores_csv) { Rails.root.join('spec', 'features', 'files', 'scores.csv') }
+    given! (:event) { FactoryGirl.create :event }
+    given (:scores_csv) { Rails.root.join 'spec', 'features', 'files', 'scores.csv' }
+    given! (:angler) { FactoryGirl.create :user_profile, first_name: 'Cory', last_name: 'Johnston' }
+    given! (:co_angler) { FactoryGirl.create :user_profile, first_name: 'Chris', last_name: 'Johnston' }
+    given! (:team) { FactoryGirl.create :team }
+
+    before do
+        team.users << angler.user
+        team.users << co_angler.user
+    end
 
     scenario 'I import a score' do
         visit '/scores'
@@ -17,5 +25,19 @@ feature 'Score Import' do
         expect(page.body).to have_content 'CORY JOHNSTON'
         expect(page.body).to have_content 'CHRIS JOHNSTON'
         expect(page.body).to have_content '42639102'
+        expect(page).to have_content "First name matches:#{angler.first_name} #{angler.last_name}"
+
+        click_link "#{angler.first_name} #{angler.last_name}", match: :first
+        expect(page).to have_content "✓ #{angler.first_name} #{angler.last_name}"
+
+        expect(page).to have_content "First name matches:#{co_angler.first_name} #{co_angler.last_name}"
+
+        click_link "#{co_angler.first_name} #{co_angler.last_name}", match: :first
+        expect(page).to have_content "✓ #{co_angler.first_name} #{co_angler.last_name}"
+
+        # TODO: Team these guys up and get this working
+        # visit '/leaderboard'
+        # expect(page).to have_content "#{angler.first_name} #{angler.last_name}"
+        # expect(page).to have_content "#{co_angler.first_name} #{co_angler.last_name}"
     end
 end
