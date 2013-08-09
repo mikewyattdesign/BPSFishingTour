488ce7ebce6ba15f6d779d4e1b4a6e79dbb93a95
Adam Bowen <adambowen@group360.com>
2013-08-09 13:16:05 -0500

    mod 7 on profile pictures

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
     factory :profile, aliases: [:user_profile] do
         association     :user, factory: :user
-        sequence(:first_name) { |n| "Joe#{n}"}            
-        sequence(:last_name) { |n| "Degrassi #{n}"}    
+        sequence(:first_name) { |n| "Joe#{n}"}
+        sequence(:last_name) { |n| "Degrassi #{n}"}
         street_address '1234 Main Street'
         city           'St. Louis'
         zip            '63112'
         state          'MO'
         phone          '5555551234'
-        sequence(:picture)         { |n| File.open(Rails.root.join('spec', 'features', 'files', "images#{n}.jpeg"))}
+        sequence(:picture)         { |n| File.open(Rails.root.join('spec', 'features', 'files', "images#{n % 7}.jpeg"))}
     end
 end
