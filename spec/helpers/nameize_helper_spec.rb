984c065ad6f7d67fd32b965a29cd977ced937adc
Mike Wyatt II <mike@mikewyattdesign.com>
2013-08-09 12:00:06 -0500

    FISHING-437 #resolve #time 7m #comment Test Driven Development for the win.

---
+++ spec/helpers/nameize_helper_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'nameize function' do
+    it 'should capitalize strings' do
+        new_string = "lowercase".nameize
+        expect(new_string).to eq("Lowercase")
+    end
+
+    it 'should capitalize after hyphens' do
+        new_string = "lower-case".nameize
+        expect(new_string).to eq("Lower-Case")
+    end
+
+    it 'should work correctly with Mc and Mac' do
+        new_string = "mcdonald"
+        expect(new_string).to eq("McDonald")
+        new_string = "macdonald"
+        expect(new_string).to eq("MacDonald")
+    end
+end
