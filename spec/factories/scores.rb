ab2004c71fe628e70969d985da60bff83b39df09
Mike Wyatt II <mike@mikewyattdesign.com>
2013-08-12 13:11:30 -0500

    FISHING-435 #resolve #time 45m #comment :score_with_team creates score, with a team of 2 assigned to the score, as well as an event

--- spec/factories/scores.rb
+++ spec/factories/scores.rb
@@ -2,14 +2,40 @@
 
 FactoryGirl.define do
   factory :score do
-    event_id 1
-    position 1
-    angler "MyString"
+    # t.integer   "event_id"
+    # t.integer   "position"
+    # t.string    "angler"
+    # t.string    "angler_rewards"
+    # t.string    "co_angler"
+    # t.string    "co_angler_rewards"
+    # t.decimal   "weight"
+    # t.decimal   "bf"
+    # t.decimal   "hp"
+    # t.integer   "angler_id"
+    # t.integer   "co_angler_id"
+    # t.integer   "team_id"
+
+    event { FactoryGirl.create(:event) }
+    sequence(:position, 1)
+    # angler_user { FactoryGirl.create(:user_with_profile) }
+    # co_angler_user { FactoryGirl.create(:user_with_profile) }
     angler_rewards "MyString"
-    co_angler "MyString"
     co_angler_rewards "MyString"
     weight "9.99"
     bf "9.99"
     hp "9.99"
+
+    factory :score_with_team do
+        before(:create) do |score|
+            FactoryGirl.create(:team_with_users)
+        end
+        after(:create) do |score|
+            score.update(angler_id: Team.last.users.first.id)
+            score.update(co_angler_id: Team.last.users.first.teammate.id)
+            score.update(angler: score.angler_user.full_name)
+            score.update(co_angler: score.co_angler_user.full_name)
+
+        end
+    end
   end
 end
--- spec/features/leaderboard_spec.rb
+++ spec/features/leaderboard_spec.rb
@@ -5,4 +5,10 @@ feature "Leaderboard" do
         visit '/leaderboard'
         expect(page).to have_content 'Leaderboard'
     end
+
+    scenario 'I expect to see scores' do
+        score = FactoryGirl.create(:score_with_team)
+        visit '/leaderboard'
+        expect(page).to have_content 'Total Score'
+    end
 end
