7120b4b44e758f6eb9aae7e1be01a983f6cc5cf9
Mike Wyatt II <mike@mikewyattdesign.com>
2013-08-12 13:13:03 -0500

    use user_with_profile factory within team_with_users factory to avoid extra profile and user creation

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
     factory :team do
 		sequence(:team_picture)		{ |n| File.open(Rails.root.join('spec', 'features', 'files', "images#{n%7}.jpeg"))}
-		
-		factory :team_with_users do 
-			users {[FactoryGirl.create(:profile).user, FactoryGirl.create(:profile).user]}
+
+		factory :team_with_users do
+			users {[FactoryGirl.create(:user_with_profile), FactoryGirl.create(:user_with_profile)]}
 		end
 
     end
