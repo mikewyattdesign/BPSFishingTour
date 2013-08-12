def9aada09f9d57575d21e1fda3fc9839f73c756
Mike Wyatt II <mike@mikewyattdesign.com>
2013-08-12 13:41:34 -0500

    FISHING-453 #resolve #time 24m #comment event_with_scores factory created with documentation for usage

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -6,5 +6,17 @@ FactoryGirl.define do
         country "Canada"
         postal_code "123456"
         date Time.now
+
+        # :event_with_scores
+        # usage: FactoryGirl.create(:event_with_scores, name: "Gateway Classic"), FactoryGirl.create(:event_with_scores, scores: 5)
+
+        factory :event_with_scores do
+            ignore do
+                scores 10
+            end
+            after(:create) do |event, evaluator|
+                FactoryGirl.create_list(:score_with_team, evaluator.scores, event: event)
+            end
+        end
     end
 end
